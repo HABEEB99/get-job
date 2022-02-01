2e41b60c07e0caf025b1e9828da2e662acfbe1e3
HABEEB <habeebahmadu1@gmail.com>
2022-02-01 16:04:47 +0100

    installed next-js and tailwind css

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>GET-JOB</title>
+        <meta name="description" content="A Linked-In like job portal" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Linkeed in </h1>
+    </div>
+  );
+}
